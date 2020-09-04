372775209f5e72b292d7536286aa516efc4f06a0
Danyel Suxdorf <d.suxdorf@student.uni-luebeck.de>
2020-09-04 22:23:41 +0200

    small corrections

--- js/lazyload.js
+++ js/lazyload.js
@@ -29,19 +29,25 @@ function createPicture(picture, path) {
           
           if (sext == "_large") {
             source.setAttribute("media", "(min-width: 960px)");
+            source.setAttribute("srcset", "".concat(path).concat(sext).concat(".").concat(text).concat(" 700w, ").concat(path).concat(sext).concat("@2x.")
+                                          .concat(text).concat(" 1400w"));
           } else if (sext == "_medium") {
             source.setAttribute("media", "(min-width: 600px)");
+            source.setAttribute("srcset", "".concat(path).concat(sext).concat(".").concat(text).concat(" 500w, ").concat(path).concat(sext).concat("@2x.")
+                                          .concat(text).concat(" 1000w"));
+          } else if (sext == "_small") {
+            /*source.setAttribute("srcset", "".concat(path).concat(sext).concat(".").concat(text).concat(" 300w, ").concat(path).concat(sext).concat("@2x.")
+                                          .concat(text).concat(" 600w, ").concat(path).concat(sext).concat("@3x.").concat(text).concat(" 900w"));*/
+            source.setAttribute("srcset", "".concat(path).concat(sext).concat(".").concat(text).concat(" 300w, ").concat(path).concat(sext).concat("@2x.")
+                                          .concat(text).concat(" 600w"));
           }
-          
-          source.setAttribute("srcset", "".concat(path).concat(sext).concat(".").concat(text).concat(" 700w, ").concat(path).concat(sext).concat("@2x.")
-                                          .concat(text).concat(" 1400w, ").concat(path).concat(sext).concat("@3x.").concat(text).concat(" 2100w"));
           source.setAttribute("sizes", sizes);
           picture.appendChild(source);
       });
   });
   var image = document.createElement("img");
-  image.setAttribute("srcset", "".concat(path).concat("_large.png 700w, ").concat(path).concat("_large@2x.png 1400w, ").concat(path).concat("_large@3x.png 2100w"));
-  image.setAttribute("src", "".concat(path).concat("_large@3x.png"));
+  image.setAttribute("srcset", "".concat(path).concat("_large.png 700w, ").concat(path).concat("_large@2x.png 1400w"));
+  image.setAttribute("src", "".concat(path).concat("_large@2x.png"));
   image.setAttribute("alt", "Illustration");
   picture.appendChild(image);
 }
