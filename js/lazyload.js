7d0c60ec65a08c18cc57ccf864bdd4f7d43e416f
Danyel Suxdorf <d.suxdorf@student.uni-luebeck.de>
2020-10-26 16:53:00 +0100

    split code into logical parts

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,72 +1,9 @@
-function masonryLayout() {
-  // init colcade
-  var colcade = new Colcade( '.grid', {
-    columns: '.grid-col',
-    items: '.grid-item'
-});
-}
-
-function createPicture(picture, path) {
-  // check vw
-  var vw = window.innerWidth;
-  var sizes;
-  if (vw < 300) {
-      sizes = "100vw";
-  } else if (vw < 768) {
-      sizes = "50vw";
-  } else if (vw < 1200) {
-      sizes = "33vw";
-  } else {
-      sizes = "25vw";
-  }
-  
-  //var sizesext = ["_large", "_medium", "_small"];
-  var typeext = ["webp", "png"];
-  //sizesext.forEach(function(sext) {
-  //    typeext.forEach(function(text) {
-  //        var source = document.createElement("source");
-  //        source.setAttribute("type", "image/".concat(text));
-  //        
-  //        if (sext == "_large") {
-  //          source.setAttribute("media", "(min-width: 960px)");
-  //          source.setAttribute("srcset", "".concat(path).concat(sext).concat(".").concat(text).concat(" 700w, ").concat(path).concat(sext).concat("@2x.")
-  //                                        .concat(text).concat(" 1400w"));
-  //        } else if (sext == "_medium") {
-  //          source.setAttribute("media", "(min-width: 600px)");
-  //          source.setAttribute("srcset", "".concat(path).concat(sext).concat(".").concat(text).concat(" 500w, ").concat(path).concat(sext).concat("@2x.")
-  //                                        .concat(text).concat(" 1000w"));
-  //        } else if (sext == "_small") {
-  //          /*source.setAttribute("srcset", "".concat(path).concat(sext).concat(".").concat(text).concat(" 300w, ").concat(path).concat(sext).concat("@2x.")
-  //                                        .concat(text).concat(" 600w, ").concat(path).concat(sext).concat("@3x.").concat(text).concat(" 900w"));*/
-  //          source.setAttribute("srcset", "".concat(path).concat(sext).concat(".").concat(text).concat(" 300w, ").concat(path).concat(sext).concat("@2x.")
-  //                                        .concat(text).concat(" 600w"));
-  //        }
-  //        source.setAttribute("sizes", sizes);
-  //        picture.appendChild(source);
-  //    });
-  //});
-  
-  typeext.forEach(function(text) {
-          var source = document.createElement("source");
-          source.setAttribute("type", "image/".concat(text));
-          source.setAttribute("srcset", "".concat(path).concat("_large@2x.").concat(text).concat(" 1400w"));
-          source.setAttribute("sizes", sizes);
-          picture.appendChild(source);
-      });
-  
-  var image = document.createElement("img");
-  image.setAttribute("srcset", "".concat(path).concat("_large@2x.png 1400w"));
-  image.setAttribute("src", "".concat(path).concat("_large@2x.png"));
-  image.setAttribute("alt", "Illustration");
-  picture.appendChild(image);
-}
-
 document.addEventListener("DOMContentLoaded", function() {
     // JS Infobox
     var jsinfo = document.getElementsByClassName("jsinfo")[0];
     jsinfo.classList.add("hidden");
 
-    masonryLayout();
+    masonryLayout(); // masonry.js
     //var colcadegrid = document.getElementsByClassName(".grid")[0];
     new SimpleLightbox({elements: '.grid a'});
     
@@ -81,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
             var picture = entry.target;
             var path = picture.dataset.src;
 
-            createPicture(picture, path);
+            createPicture(picture, path); // cpe.js
             
             picture.classList.remove("lazy");
             imageObserver.unobserve(picture);
@@ -110,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function() {
               if(picture.offsetTop < (window.innerHeight + scrollTop)) {
                 var path = picture.dataset.src;
 
-                createPicture(picture, path);
+                createPicture(picture, path); // cpe.js
 
                 picture.classList.remove('lazy');
               }
--- js/masonry.js
+++ js/masonry.js
@@ -1,8 +1,10 @@
-// init colcade
-var colcade = new Colcade( '.grid', {
-    columns: '.grid-col',
-    items: '.grid-item'
-});
+function masonryLayout() {
+    // init colcade
+    var colcade = new Colcade( '.grid', {
+      columns: '.grid-col',
+      items: '.grid-item'
+  });
+  }
 
 function updateLayout() {
     this.colcade.reload;
