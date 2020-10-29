58b9175e36b40968fbd31c4ae85c81558dc6f84d
ElisabethJahnke <70760541+ElisabethJahnke@users.noreply.github.com>
2020-10-29 18:04:51 +0100

    Update videoGallery.js

--- video/videoGallery.js
+++ video/videoGallery.js
@@ -1,9 +1,22 @@
 const dataSet = [
-    ["video/IMG_0241", "Title", "video/poster.jpg"],
-    ["video/IMG_0241", "Title", "video/poster.jpg"],
-    ["video/IMG_0241", "Title", "video/poster.jpg"],
-    ["video/IMG_0241", "Title", "video/poster.jpg"],
-    ["video/IMG_0241", "Title", "video/poster.jpg"],
+    ["video/200728_Waldgeister-Gesicht", "-", "video/poster.jpg"],
+    ["video/200728_Waldgeister-Frosch", "-", "video/poster.jpg"],
+    ["video/200728_Waldgeister-Hand", "-", "video/poster.jpg"],
+    ["video/200728_Waldgeister-Octopus", "-", "video/poster.jpg"],
+    ["video/200728_Waldgeister-Augen", "-", "video/poster.jpg"],
+    ["video/200728_Waldgeister-Herz", "-", "video/poster.jpg"]
+    ["video/200728_Waldgeister-Wave-Pilz", "-", "video/poster.jpg"]
+    ["video/200728_Waldgeister-Pilze", "-", "video/poster.jpg"]
+    ["video/200728_Waldgeister-Oxolotl", "-", "video/poster.jpg"]
+    ["video/200728_Waldgeister-Drache", "-", "video/poster.jpg"]
+    ["video/200728_Waldgeister-Graeser", "-", "video/poster.jpg"]
+    ["video/200728_Waldgeister-lava", "-", "video/poster.jpg"]
+    ["video/200728_Waldgeister-Pilze", "-", "video/poster.jpg"]
+    ["video/200728_Waldgeister-Pusteblume", "-", "video/poster.jpg"]
+    ["video/200728_Waldgeister-Tannenzapfen", "-", "video/poster.jpg"]
+    ["video/200728_Waldgeister-Pancake-Pilz", "-", "video/poster.jpg"]
+    ["video/200728_Waldgeister-Baumpilz", "-", "video/poster.jpg"]
+
     ["video/IMG_0241", "Title", "video/poster.jpg"]
 ];
 
